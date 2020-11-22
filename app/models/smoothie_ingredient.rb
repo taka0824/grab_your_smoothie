64b243569155af0b712bbed76cd2ba7aad10acd8
Takanari <taka0824nari@gmail.com>
2020-11-22 11:35:19 +0000

    smoothie show bug fixed

--- app/models/smoothie_ingredient.rb
+++ app/models/smoothie_ingredient.rb
@@ -6,24 +6,24 @@ class SmoothieIngredient < ApplicationRecord
   def self.total_nutrient_to_gram(vitamin)
       array = []
       self.all.each do |smoothie_ingredient|
-        array << smoothie_ingredient.ingredient[vitamin].to_f * smoothie_ingredient.amount
+        array << (smoothie_ingredient.ingredient[vitamin].to_f / 100) * smoothie_ingredient.amount
       end
-      (array.sum / 100).floor(3)
+      array.sum.round(3)
   end
 
   def self.total_nutrient_to_mili(vitamin)
     array = []
     self.all.each do |smoothie_ingredient|
-      array << smoothie_ingredient.ingredient[vitamin].to_f * 1000 * smoothie_ingredient.amount
+      array << (smoothie_ingredient.ingredient[vitamin].to_f / 100 * 1000) * smoothie_ingredient.amount
     end
-    (array.sum / 100).floor(3)
+    array.sum.round(3)
   end
 
   def self.total_nutrient_to_micro(vitamin)
     array = []
     self.all.each do |smoothie_ingredient|
-      array << smoothie_ingredient.ingredient[vitamin].to_f * 1000000 * smoothie_ingredient.amount
+      array << (smoothie_ingredient.ingredient[vitamin].to_f / 100 * 1000000) * smoothie_ingredient.amount
     end
-    (array.sum / 100).floor(3)
+    array.sum.floor(3)
   end
 end
