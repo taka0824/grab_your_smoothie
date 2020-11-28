bb693954ff2dffb0954f5169d4151ac93fa6da66
Takanari <taka0824nari@gmail.com>
2020-11-28 05:51:11 +0000

    unnecessary migration file deleted

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 
   def top
     @all_ranks = Smoothie.joins(:favorites).where(favorites: {created_at: Time.now.all_month}).group(:id).order('count(favorites.smoothie_id) desc').limit(3)
-    @new_smoothies = Smoothie.all.order(created_at: "DESC").limit(5)
+    @new_smoothies = Smoothie.all.order(created_at: "DESC").limit(6)
   end
 
   def about
--- db/migrate/20201119040221_remove_name_hiragana_from_ingredients.rb
+++
@@ -1,8 +0,0 @@
-class RemoveNameHiraganaFromIngredients < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :ingredients, :name_kanji_string, :string
-  end
-  def change
-    remove_column :ingredients, :name_hiragana_string, :string
-  end
-end
