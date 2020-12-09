14a647bd06953564deca740341ef7d567fefc3bf
Takanari <taka0824nari@gmail.com>
2020-12-09 14:21:00 +0000

    database changed

--- db/migrate/20201118042646_create_ingredients.rb
+++ db/migrate/20201118042646_create_ingredients.rb
@@ -2,19 +2,19 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.decimal :energy, precision: 13, scale: 9, default: 0.0
-      t.decimal :protein, precision: 13, scale: 9, default: 0.0
-      t.decimal :carb, precision: 13, scale: 9, default: 0.0
-      t.decimal :lipid, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_a, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_b1, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_b2, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_b6, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_b12, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_c, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_d, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_e, precision: 13, scale: 9, default: 0.0
-      t.decimal :vitamin_k, precision: 13, scale: 9, default: 0.0
+      t.decimal :energy, precision: 13, scale: 9
+      t.decimal :protein, precision: 13, scale: 9
+      t.decimal :carb, precision: 13, scale: 9
+      t.decimal :lipid, precision: 13, scale: 9
+      t.decimal :vitamin_a, precision: 13, scale: 9
+      t.decimal :vitamin_b1, precision: 13, scale: 9
+      t.decimal :vitamin_b2, precision: 13, scale: 9
+      t.decimal :vitamin_b6, precision: 13, scale: 9
+      t.decimal :vitamin_b12, precision: 13, scale: 9
+      t.decimal :vitamin_c, precision: 13, scale: 9
+      t.decimal :vitamin_d, precision: 13, scale: 9
+      t.decimal :vitamin_e, precision: 13, scale: 9
+      t.decimal :vitamin_k, precision: 13, scale: 9
       t.integer :created_by
 
       t.timestamps
