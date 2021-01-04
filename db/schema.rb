aeee1ea7b5df6b3ba8aa9dd9dfee5ecc50f987ee
Takanari <taka0824nari@gmail.com>
2021-01-04 02:41:03 +0000

    before migration file edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_042309) do
+ActiveRecord::Schema.define(version: 2021_01_03_095357) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 2020_12_16_042309) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rule_violation_number", default: 0
+    t.datetime "discarded_at"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
