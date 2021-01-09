ac8432a59ea3d0c2abe2f9a019e29eab98a962ee
Takanari <taka0824nari@gmail.com>
2021-01-09 09:08:43 +0000

    rank_with_favorite method created at smoothie model

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,8 @@ class HomesController < ApplicationController
   def top
     # @all_ranks = Smoothie.joins(:favorites).where(favorites: {created_at: Time.now.all_month}).group(:id).order('count(favorites.smoothie_id) desc').limit(3)
     # 上の記述は今月分のいいねだけを対象
-    @all_ranks = Smoothie.joins(:favorites).group(:id).order('count(favorites.smoothie_id) desc').limit(3)
+    @all_ranks = Smoothie.rank_with_favorite.limit(3)
     @new_smoothies = Smoothie.all.order(created_at: "DESC").limit(6)
   end
-  
+
 end
